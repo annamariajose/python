c8dc7e3b5f29d936a68f3202559d10b17b415e88
annamariajose <annajose291196@gmail.com>
2018-11-01 11:40:43 +0530

    initial commit

---
+++ sg.py
@@ -0,0 +1,11 @@
+class Str:
+...     def __init__(self, value):
+...             self.value=value
+...     def print (self):
+...             obj=self.value.split(" ")
+...             print (obj)
+sc=input()
+1 3 5 6 7
+>>> sc=Str(sc)
+>>> sc.print()
+
